310e77a13f223a35babe954ffcecb4f0491f2b42
Fernando Goncalves <fc47860@alunos.fc.ul.pt>
2020-10-11 12:10:33 +0100

    persistencia de sessao em ficheiro log

--- Server_RAFT/src/main/Server.java
+++ Server_RAFT/src/main/Server.java
@@ -1,6 +1,11 @@
 package main;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import enums.State;
 
@@ -16,6 +21,9 @@ public class Server implements IServer{
 		this.state = State.FOLLOWER;
 		this.table = new ArrayList<>();
 		lastEntry = 0;
+		String diretoria = System.getProperty("user.dir");
+		File ficheiroLog = new File(diretoria + "/log.txt");
+		ficheiroLog.delete();
 	}
 
 	public void leader() {
@@ -28,8 +36,30 @@ public class Server implements IServer{
 		int idOperacao = Integer.parseInt(dividir[1]);
 		table.add(s+":"+idCliente+":"+idOperacao);
 		lastEntry++;
-		System.out.println("Operacao: " + idOperacao +", Cliente: " + idCliente + ", String no log: " + s);
+		String log = "Operacao: " + idOperacao +", Cliente: " + idCliente + ", String no log: " + s;
+		System.out.println(log);
+		try {
+			escreveLog(log);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return s;
+	}
+
+	private void escreveLog(String log) throws IOException {
+		String diretoria = System.getProperty("user.dir");
+		File ficheiroLog = new File(diretoria + "/log.txt");
+		ficheiroLog.createNewFile();
+		Scanner sc = new Scanner(ficheiroLog);
+		while(sc.hasNextLine()) {
+			sc.nextLine();
+			
+		}
+		FileOutputStream fos0 = new FileOutputStream(ficheiroLog,true);
+		fos0.write(log.getBytes());
+		fos0.write("\n".getBytes());
+		sc.close();
+		fos0.close();
 	}
 
 	public int getPort() {
